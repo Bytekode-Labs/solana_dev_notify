515a18c6f1bcdad24cf275ec5a6fc3898f10ce6d
Shlok D. Mange <shlok_mange@protonmail.com>
2023-05-02 14:39:45 +0530

    wallet tracking

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,28 @@
 import express from 'express'
+import { config } from 'dotenv'
+import { Connection, PublicKey, LAMPORTS_PER_SOL } from '@solana/web3.js'
 import { urlencoded } from 'body-parser'
+config()
 const app = express()
 
 app.use(express.json())
 app.use(urlencoded({ extended: false }))
 app.listen(process.env.PORT || 8080)
 
-app.post('/notify', async (req, res) => {
-    const { asset, amount, } = await req.body
+const WSS_ENDPOINT = process.env.WS_PROVIDER as string
+const HTTP_ENDPOINT = process.env.HTTP_PROVIDER as string
+
+const solanaConnection = new Connection(HTTP_ENDPOINT, {
+    wsEndpoint:WSS_ENDPOINT
 })
+
+const trackWallet = async () => {
+    const wallet = new PublicKey(`FNhPWcjJ98ZVbadvGYktR4soc74pEXw4WhSAmy48tYu7`)
+    const subscriptionId = await solanaConnection.onAccountChange(wallet, 
+        (updatedLog) => {
+            console.log(updatedLog)
+        }
+    )
+}
+
+trackWallet()
