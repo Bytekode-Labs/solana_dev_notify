b78951f9630b288e30e3683072da705c573d7415
Shlok D. Mange <shlok_mange@protonmail.com>
2023-05-08 13:18:55 +0530

    v0

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import express from 'express'
 import { config } from 'dotenv'
 import { Connection, PublicKey, LAMPORTS_PER_SOL } from '@solana/web3.js'
 import { urlencoded } from 'body-parser'
-import { deserialize } from 'v8'
+import { telegramBot } from './config/telegram'
 config()
 const app = express()
 
@@ -12,6 +12,7 @@ app.listen(process.env.PORT || 8080)
 
 const WSS_ENDPOINT = process.env.WS_PROVIDER as string
 const HTTP_ENDPOINT = process.env.HTTP_PROVIDER as string
+const chatId = process.env.CHAT_ID as unknown as number
 
 const solanaConnection = new Connection(HTTP_ENDPOINT, {
     wsEndpoint:WSS_ENDPOINT
@@ -32,10 +33,14 @@ const trackWallet = async () => {
             let asset = `SOL`
             const accountKeys = sig?.transaction.message.accountKeys 
             let message = `Successfully transferred ${amount/LAMPORTS_PER_SOL} ${asset} to ${accountKeys![1].pubkey.toString()}`
-            console.log(accountKeys)
+            telegramBot.sendMessage(chatId, message)
             console.log(message)
         }
     )
 }
 
 trackWallet()
+
+telegramBot.on('message', (message) => {
+    console.log(message.chat.id)
+})
